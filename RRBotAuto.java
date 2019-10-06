750877f8208da384e228b775d2fc9f8778eaf9bb
John <john@jbrereton.com>
2019-10-06 16:11:30 -0400

    bertone

--- RRBotAuto.java
+++ RRBotAuto.java
@@ -1,100 +1,175 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import java.util.Locale;
 
+@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@Disabled
+public class RRBotAuto extends LinearOpMode {
 
-
-@Autonomous(name="Basic: Iterative OpMode", group="Iterative Opmode")
-public class RRBotAuto extends OpMode
-{
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     RRBotHardware robot = new RRBotHardware();
 
+    //gyro variables
+    private BNO055IMU imu;
+    private Orientation angles;
+
+    //Motor Definitions
+    private final double COUNTS_PER_MOTOR_REV = 537.6 ;    // Andymark 20:1 gearmotor
+    private final double DRIVE_GEAR_REDUCTION = 1.0 ;     // This is < 1.0 if geared UP
+    private final double WHEEL_DIAMETER_INCHES = 4.0 ;     // For figuring circumference
+    private final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+
+
     //construct drive class
     RRBotMecanumDrive drive = new RRBotMecanumDrive(robot);
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
     @Override
-    public void init() {
+    public void runOpMode() {
         telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
-        // Tell the driver that initialization is complete.
-        telemetry.addData("Status", "Initialized");
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
-
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
         runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+    public void EncoderDriveTank(double speed, double leftInches, double rightInches, double timeoutS)
+    {
+        robot.rearLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rearRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        idle();
 
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
+        robot.rearLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rearRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        int newRearLeftTarget;
+        int newRearRightTarget;
+        int newFrontLeftTarget;
+        int newFrontRightTarget;
 
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
+        // Ensure that the opmode is still active
+        if (opModeIsActive())
+        {
+            // Determine new target position, and pass to motor controller
+            newRearLeftTarget = robot.rearLeftMotor.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRearRightTarget = robot.rearRightMotor.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            newFrontLeftTarget = robot.frontLeftMotor.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newFrontRightTarget = robot.frontRightMotor.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            robot.rearLeftMotor.setTargetPosition(newRearLeftTarget);
+            robot.rearRightMotor.setTargetPosition(newRearRightTarget);
+            robot.frontLeftMotor.setTargetPosition(newFrontLeftTarget);
+            robot.frontRightMotor.setTargetPosition(newFrontRightTarget);
 
-        // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
+            // Turn On RUN_TO_POSITION
+            robot.rearLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rearRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.rearLeftMotor.setPower(Math.abs(speed));
+            robot.rearRightMotor.setPower(Math.abs(speed));
+            robot.frontLeftMotor.setPower(Math.abs(speed));
+            robot.frontRightMotor.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            while(opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.rearLeftMotor.isBusy() && robot.rearRightMotor.isBusy() && robot.frontLeftMotor.isBusy() && robot.frontRightMotor.isBusy()))
+            {
+
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newRearLeftTarget, newRearRightTarget, newFrontLeftTarget, newFrontRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        robot.rearLeftMotor.getCurrentPosition(),
+                        robot.rearRightMotor.getCurrentPosition(),
+                        robot.frontLeftMotor.getCurrentPosition(),
+                        robot.frontRightMotor.getCurrentPosition());
+                telemetry.update();
+            }
+
+            TurnOffMotors();
+
+            // Turn off RUN_TO_POSITION
+            robot.rearLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rearRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
+    public void TurnByGyro(String direction, int angle, double speed)
+    {
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        float startHeading = angles.firstAngle;
+
+        if(direction.equals("left"))
+        {
+            robot.rearRightMotor.setPower(speed);
+            robot.rearLeftMotor.setPower(-speed);
+            robot.frontRightMotor.setPower(speed);
+            robot.frontLeftMotor.setPower(-speed);
+        }
+        else if(direction.equals("right"))
+        {
+            robot.rearRightMotor.setPower(-speed);
+            robot.rearLeftMotor.setPower(speed);
+            robot.frontRightMotor.setPower(-speed);
+            robot.frontLeftMotor.setPower(speed);
+        }
+
+        while(opModeIsActive() && Math.abs(angles.firstAngle - startHeading) < angle)
+        {
+            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            telemetry.addData("heading", formatAngle(AngleUnit.DEGREES, angles.firstAngle));
+            telemetry.update();
+        }
+
+        TurnOffMotors();
+    }
+    String formatAngle(AngleUnit angleUnit, double angle)
+    {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
     }
 
+    String formatDegrees(double degrees)
+    {
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+    public void TurnOffMotors()
+    {
+        robot.rearRightMotor.setPower(0);
+        robot.rearLeftMotor.setPower(0);
+        robot.frontRightMotor.setPower(0);
+        robot.frontLeftMotor.setPower(0);
+    }
 }
